d8c10ae2656e38b2546d11a50d09b8493e7af68f
Avinash <Avinashmate995@gmail.com>
2023-09-05 01:01:18 +0530

    solved

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -1,7 +1,4 @@
 package com.driver.controllers;
-
-import com.driver.model.Booking;
-package com.driver.controllers;
 import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
@@ -18,16 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-@RestController
-import org.springframework.web.bind.annotation.PutMapping;
-        import org.springframework.web.bind.annotation.RequestBody;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
-        import java.util.HashMap;
-        import java.util.List;
-        import java.util.Objects;
-        import java.util.UUID;
-
 
 @RestController
 @RequestMapping("/hotel")
@@ -42,19 +29,15 @@ public class HotelManagementController {
         //You need to add an hotel to the database
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
-        //You need to add an hotel to the database
-        //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
-        //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
 
-        return null;
+
         return hotelManagementService.addHotel(hotel);
     }
 
     @PostMapping("/add-user")
     public Integer addUser(@RequestBody User user){
-
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
@@ -64,7 +47,6 @@ public class HotelManagementController {
 
     @GetMapping("/get-hotel-with-most-facilities")
     public String getHotelWithMostFacilities(){
-
         //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
@@ -75,7 +57,6 @@ public class HotelManagementController {
 
     @PostMapping("/book-a-room")
     public int bookARoom(@RequestBody Booking booking){
-
         //The booking object coming from postman will have all the attributes except bookingId and amountToBePaid;
         //Have bookingId as a random UUID generated String
         //save the booking Entity and keep the bookingId as a primary key
@@ -94,6 +75,7 @@ public class HotelManagementController {
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
+        //In this function return the bookings done by a person
 
         //In this function return the bookings done by a person
         return hotelManagementService.getBookings(aadharCard);
@@ -101,7 +83,6 @@ public class HotelManagementController {
 
     @PutMapping("/update-facilities")
     public Hotel updateFacilities(List<Facility> newFacilities,String hotelName){
-
         //We are having a new facilites that a hotel is planning to bring.
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
