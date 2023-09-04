860524571e113712859ca299c91dd9d979ff3945
Avinash <Avinashmate995@gmail.com>
2023-09-04 23:49:43 +0530

    solved

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -14,8 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-
-
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
